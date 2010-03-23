52fe2a27d21eb62a880fe3979dc12ee6de733f55
wismerst <wismerst@05686622-51ee-4c29-b765-5a36481f58a2>
2010-03-23 23:00:54 +0000

    SW: added a working copy of can-serial
    git-svn-id: svn+ssh://svn.aslforge.ethz.ch/svnroot/avalon@42 05686622-51ee-4c29-b765-5a36481f58a2

--- cmake_install.cmake
+++ cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/krucker/Master_thesis/avalon
+# Install script for directory: /home/stefan/RoboYacht/svn/avalon
 
 # Set the install prefix
 IF(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -38,7 +38,7 @@ ELSE(CMAKE_INSTALL_COMPONENT)
   SET(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
 ENDIF(CMAKE_INSTALL_COMPONENT)
 
-FILE(WRITE "/home/krucker/Master_thesis/avalon/${CMAKE_INSTALL_MANIFEST}" "")
+FILE(WRITE "/home/stefan/RoboYacht/svn/avalon/${CMAKE_INSTALL_MANIFEST}" "")
 FOREACH(file ${CMAKE_INSTALL_MANIFEST_FILES})
-  FILE(APPEND "/home/krucker/Master_thesis/avalon/${CMAKE_INSTALL_MANIFEST}" "${file}\n")
+  FILE(APPEND "/home/stefan/RoboYacht/svn/avalon/${CMAKE_INSTALL_MANIFEST}" "${file}\n")
 ENDFOREACH(file)
