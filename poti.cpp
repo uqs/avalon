6fc408685683741a76c08e1db35c6553398cf96f
kruckema <kruckema@05686622-51ee-4c29-b765-5a36481f58a2>
2010-05-25 12:21:54 +0000

    nomsg
    git-svn-id: svn+ssh://svn.aslforge.ethz.ch/svnroot/avalon@78 05686622-51ee-4c29-b765-5a36481f58a2

---
+++ poti.cpp
@@ -0,0 +1,29 @@
+/**
+ *	This reads the target angles form the Store and sets it on the Rudder-EPOS
+ *
+ **/
+
+// General Things
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+// Specific Things
+#include "can/can.h"
+#include "epos/epos.h"
+
+int
+main (int argc, char * argv[])
+{
+  can_init("/dev/ttyUSB0");
+
+  while(1) {
+    can_read_message();
+    if (epos_read.node[0].msg_id != 0)
+      printf("Received 0x%X", epos_read.node[0].msg_id);
+  }
+
+  can_close();
+  return (0);
+}
+
