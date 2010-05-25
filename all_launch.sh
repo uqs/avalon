4de3fbc175efa1521d8cc3fa5ab56bc43748a2a9
kruckema <kruckema@05686622-51ee-4c29-b765-5a36481f58a2>
2010-05-25 12:40:59 +0000

    git-svn-id: svn+ssh://svn.aslforge.ethz.ch/svnroot/avalon@79 05686622-51ee-4c29-b765-5a36481f58a2

--- all_launch.sh
+++ all_launch.sh
@@ -4,5 +4,5 @@ launch -c launch.conf &
 sleep 5
 ./destination_converter
 sleep 1
-matlab -nodisplay -r "addpath ~/Master_thesis/matlab/Matlab_obst_avoid_010410_new;simulationOne_shell; exit"
+matlab -nodisplay -r "addpath matlab_091217;simulationOne_shell; exit"
 
