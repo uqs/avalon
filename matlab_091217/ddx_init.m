d94840b195c62413f9c831a48d4452f32c1fcc91
kruckema <kruckema@05686622-51ee-4c29-b765-5a36481f58a2>
2010-06-08 13:52:46 +0000

    nomsg
    git-svn-id: svn+ssh://svn.aslforge.ethz.ch/svnroot/avalon@100 05686622-51ee-4c29-b765-5a36481f58a2

--- matlab_091217/ddx_init.m
+++ matlab_091217/ddx_init.m
@@ -11,8 +11,8 @@ if isempty(robot_variable)
     GPSDATA_MSG          = 'struct { struct {double longitude; double latitude; double altitude;} position; double speed_kmph; double speed_kn; double course; short dgpsage; short dgpsref; float HDOP; short gpsFIX; int sat; } gpsData';
     JOYSTICK_MSG         = 'struct { int nb; int na; int axes[12]; int buttons[12];} joystick';
     RUDDERTARGET_MSG     = 'struct { double torque_des; double degrees_left; double degrees_right; int resetleft_request; int resetright_request;} rudderTarget';
-        SAILTARGET_MSG       = 'struct { double degrees; int reset_request;} sailTarget';
-        FLAGS_MSG            = 'struct { int man_in_charge; int state; int state_requested; int sail_direction; int sailor_no_tack_or_jibe; int navi_state; long navi_index_call; long navi_index_answer; int skip_index_dest_call; int autonom_navigation; int global_locator; int joystick_timeout; int error_state; } Flags';
+    SAILTARGET_MSG       = 'struct { float degrees; int reset_request;} sailTarget';
+        FLAGS_MSG            = 'struct { int man_in_charge; int state; int state_requested; int sail_direction; int sailor_no_tack_or_jibe; int navi_state; long navi_index_call; long navi_index_answer; int skip_index_dest_call; long skip_index_dest_answer; int autonom_navigation; int global_locator; int joystick_timeout; } Flags';
     RCFLAGS_MSG          = 'struct { int emergency_stop; int motion_stop; int joystick_timeout; int sailorstate_requested; int man_in_charge; int autonom_navigation; } rcFlags';
     SAILORFLAGS_MSG      = 'struct { int state; int no_tack_or_jibe; int sail_direction; } sailorFlags';
     NAVIFLAGS_MSG        = 'struct { int navi_state; long navi_index_call; long navi_index_answer; } NaviFlags';
@@ -21,7 +21,7 @@ if isempty(robot_variable)
     RUDDERSTATERIGHT_MSG = 'struct { float degrees_rudder; float ref_rudder; } Rudderstate';
     RUDDERSTATELEFT_MSG  = 'struct { float degrees_rudder; float ref_rudder; } Rudderstate';
          DESIREDHEADING_MSG   = 'struct { double heading; } DesiredHeading';
-    IMU_MSG              = 'struct { double speed; double theta_star; struct{ double longitude; double latitude; double altitude;} position; struct{ double roll; double pitch; double yaw;} attitude; struct{ double x; double y; double z;} velocity; struct{ double x; double y; double z;} acceleration; struct{ double x; double y; double z;} gyro; double temperature; } imuData';
+    IMU_MSG              = 'struct { double speed; struct{ double longitude; double latitude; double altitude;} position; struct{ double roll; double pitch; double yaw;} attitude; struct{ double x; double y; double z;} velocity; struct{ double x; double y; double z;} acceleration; struct{ double x; double y; double z;} gyro; double temperature; } imuData';
     IMUCLEANDATA_MSG     = 'struct { struct { double roll; } attitude; struct { double x; double y; double z; double drift; } velocity; } imuCleanData';
     AISSTRUCT_MSG        = 'struct { long mmsi; int navigational_status; double rate_of_turn; int speed_over_ground; int position_accuracy; double longitude; double latitude; double course_over_ground; double heading; char destination[21]; double time_of_arrival; double timestamp;} AisStruct ';
     AISDATA_MSG          = 'struct { int number_of_ships; AisStruct Ship[15]; } AisData';
@@ -46,7 +46,7 @@ if isempty(robot_variable)
         RegisterType( out.store, JOYSTICK_MSG );
         RegisterType( out.store, RUDDERTARGET_MSG );
         RegisterType( out.store, SAILTARGET_MSG );
-        RegisterType( out.store, FLAGS_MSG );
+            RegisterType( out.store, FLAGS_MSG );
         RegisterType( out.store, RCFLAGS_MSG );
         RegisterType( out.store, SAILORFLAGS_MSG );
         RegisterType( out.store, NAVIFLAGS_MSG );
@@ -54,15 +54,15 @@ if isempty(robot_variable)
         RegisterType( out.store, SAILSTATE_MSG );
         RegisterType( out.store, RUDDERSTATERIGHT_MSG );
         RegisterType( out.store, RUDDERSTATELEFT_MSG );
-        RegisterType( out.store, DESIREDHEADING_MSG );
+            RegisterType( out.store, DESIREDHEADING_MSG );
         RegisterType( out.store, IMU_MSG );
         RegisterType( out.store, IMUCLEANDATA_MSG );
         RegisterType( out.store, AISSTRUCT_MSG );
         RegisterType( out.store, AISDATA_MSG );
         RegisterType( out.store, DESTINATIONSTRUCT_MSG );
         RegisterType( out.store, DESTINATIONDATA_MSG );
-       RegisterType( out.store, WAYPOINTSTRUCT_MSG );
-       RegisterType( out.store, WAYPOINTDATA_MSG );
+        RegisterType( out.store, WAYPOINTSTRUCT_MSG );
+        RegisterType( out.store, WAYPOINTDATA_MSG );
         
         RegisterVariable( out.store, 'localization', 'Localization' );
         RegisterVariable( out.store, 'control', 'Control' );
@@ -72,7 +72,7 @@ if isempty(robot_variable)
         RegisterVariable( out.store, 'joystick', 'joystick' );
         RegisterVariable( out.store, 'rudder', 'rudderTarget' );
         RegisterVariable( out.store, 'sail', 'sailTarget' );
-        RegisterVariable( out.store, 'flags', 'Flags' );
+            RegisterVariable( out.store, 'flags', 'Flags' );
         RegisterVariable( out.store, 'rcflags', 'rcFlags' );
         RegisterVariable( out.store, 'sailorflags', 'sailorFlags' );
         RegisterVariable( out.store, 'naviflags', 'NaviFlags' );
@@ -80,7 +80,7 @@ if isempty(robot_variable)
         RegisterVariable( out.store, 'sailstate', 'Sailstate');
         RegisterVariable( out.store, 'rudderstateright', 'Rudderstate' );
         RegisterVariable( out.store, 'rudderstateleft', 'Rudderstate' );
-        RegisterVariable( out.store, 'desiredheading', 'DesiredHeading' );
+            RegisterVariable( out.store, 'desiredheading', 'DesiredHeading' );
         RegisterVariable( out.store, 'imu', 'imuData' );
         RegisterVariable( out.store, 'cleanimu', 'imuCleanData' );
         RegisterVariable( out.store, 'aisStruct', 'AisStruct');
@@ -98,7 +98,7 @@ if isempty(robot_variable)
         out.joystick         = Variable( out.store, 'joystick' );
         out.rudder           = Variable( out.store, 'rudder' );
         out.sail             = Variable( out.store, 'sail' );
-        out.flags            = Variable( out.store, 'flags' );
+            out.flags            = Variable( out.store, 'flags' );
         out.rcflags          = Variable( out.store, 'rcflags' );
         out.sailorflags      = Variable( out.store, 'sailorflags' );
         out.naviflags        = Variable( out.store, 'naviflags' );
@@ -106,7 +106,7 @@ if isempty(robot_variable)
         out.sailstate        = Variable( out.store, 'sailstate');
         out.rudderstateright = Variable( out.store, 'rudderstateright');
         out.rudderstateleft  = Variable( out.store, 'rudderstateleft');
-        out.desiredheading   = Variable( out.store, 'desiredheading' );
+            out.desiredheading   = Variable( out.store, 'desiredheading' );
         out.imu              = Variable( out.store, 'imu' );
         out.cleanimu         = Variable( out.store, 'cleanimu' );
         out.aisStruct        = Variable( out.store, 'aisStruct' );
